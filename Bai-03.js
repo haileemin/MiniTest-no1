1d4e96b7f59229b7e62658bee0edab5243c73cc8
haileemin <dr.leminhhai2003@gmail.com>
2023-12-04 19:16:46 +0700

    Mini-test-no1

--- Bai-03.js
+++ Bai-03.js
@@ -1,11 +1,9 @@
 // Liệt kê tất cả các số chính phương nhỏ hơn n
 
 let number = 100;
-let n;
-let arr = [];
 for (i = 1; i <= 100; i++) {
-    if (Math.sqrt(i) % 2 == 0) {
-        arr.splice(1, 0, i);
+    if (Math.sqrt(i) % 1 == 0) {
+        console.log(i);
     }
 }
-console.log(arr);
+
